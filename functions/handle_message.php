8717ece7b04c948b0cf5ec91db550ff746082156
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-30 04:19:03 +0700

    tes is private

--- functions/handle_message.php
+++ functions/handle_message.php
@@ -2,13 +2,17 @@
 function handle_message($botdata){
     $chat = $botdata["chat"];
     $chat_id = $chat["id"];
-    f("bot_kirim_perintah")("sendMessage",[
-        "chat_id"=>$chat_id,
-        "text"=>"Halo.. :D",
-    ]);
-    sleep(2);
-    f("bot_kirim_perintah")("sendMessage",[
-        "chat_id"=>$chat_id,
-        "text"=>"Hehe :D",
-    ],);
+    if(f("is_private")($botdata)){
+        //
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>"ini private",
+        ]);
+    }
+    else{
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>"ini group",
+        ]);
+    }
 }
